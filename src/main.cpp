0f2dee9385e0e4ab81d6b47aef0d8dbe57d6e8cd
kby <tetepiti149@gmail.com>
2026-02-12 14:42:46 +0900

    refactor: remove "using namespace std"

--- src/main.cpp
+++ src/main.cpp
@@ -8,28 +8,25 @@
 #include "MyUtils/Thread.h"
 #include "TestCode/MultiThreadTest.h"
 
-using namespace std;
-using namespace MyUtils;
-
 int main() {
     const int WORKER_COUNT = 5;
     const int ACTOR_COUNT = 500;
     const int TASKS_PER_ACTOR = 1000;
 
     for (int i = 0; i < WORKER_COUNT; i++) {
-        GThreadManager->Launch([=]() {
+        MyUtils::GThreadManager->Launch([=]() {
             while (true) {
-                ThreadManager::GetRegisteredActorAndProcess();
+                MyUtils::ThreadManager::GetRegisteredActorAndProcess();
                 std::this_thread::yield();
             }
         });
     }
 
-    cout << "Test Started with " << ACTOR_COUNT << " actors, each posting " << TASKS_PER_ACTOR << " tasks." << endl;
+    std::cout << "Test Started with " << ACTOR_COUNT << " actors, each posting " << TASKS_PER_ACTOR << " tasks." << std::endl;
 
-    vector<shared_ptr<Player>> players(ACTOR_COUNT);
+    std::vector<std::shared_ptr<Player>> players(ACTOR_COUNT);
     for (int i = 0; i < ACTOR_COUNT; ++i) {
-        shared_ptr<Player> playerRef = MyUtils::Memory::ObjectPool<Player>::Acquire(i);
+        std::shared_ptr<Player> playerRef = MyUtils::Memory::ObjectPool<Player>::Acquire(i);
         players[i] = playerRef;
     }
 
@@ -39,26 +36,26 @@ int main() {
         }
     }
 
-    cout << "[Test] All tasks posted. Waiting for completion..." << endl;
+    std::cout << "[Test] All tasks posted. Waiting for completion..." << std::endl;
 
     while (true) {
         long long total = 0;
         for (auto& player : players) {
             total += player->GetCount();
         }
-        cout << "\rTotal Count: " << total << " / " << (ACTOR_COUNT * TASKS_PER_ACTOR) << flush;
+        std::cout << "\rTotal Count: " << total << " / " << (ACTOR_COUNT * TASKS_PER_ACTOR) << std::flush;
         
         if (total >= (long long)ACTOR_COUNT * TASKS_PER_ACTOR) {
-            cout << endl << "[Test] All tasks completed." << endl;
+            std::cout << std::endl << "[Test] All tasks completed." << std::endl;
             players.clear();
             break;
         }
-        this_thread::sleep_for(chrono::milliseconds(100));
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 
     
 
-    GThreadManager->Join();
+    MyUtils::GThreadManager->Join();
 
     return 0;
 }
